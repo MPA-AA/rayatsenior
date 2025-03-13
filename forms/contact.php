88d2ecd0c32569f6074160df55c66566f9ce6504
MPA-AA <maira@mpandrade.uk>
2025-03-13 14:11:59 -0300

    Contact form

--- forms/contact.php
+++ forms/contact.php
@@ -1,24 +1,34 @@
 <?php
 // Replace with your real receiving email address
-$to = 'maira.andraddee@gmail.com'; 
+$to = 'contato@rayatsenior.com'; // Updated to your desired email
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get form data
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
-    $subject = htmlspecialchars($_POST['subject']);
+    $phone = htmlspecialchars($_POST['phone']);
     $message = htmlspecialchars($_POST['message']);
 
-    // Email headers
+    // Set email subject
+    $subject = "Novo Contato de $name";
+
+    // Compose the email body
+    $body = "Nome: $name\n";
+    $body .= "Email: $email\n";
+    $body .= "Telefone: $phone\n";
+    $body .= "Mensagem:\n$message";
+
+    // Set email headers
     $headers = "From: $email\r\n";
     $headers .= "Reply-To: $email\r\n";
 
     // Send email
-    if (mail($to, $subject, "Name: $name\nEmail: $email\nMessage: $message", $headers)) {
-        echo "Message sent successfully!";
+    if (mail($to, $subject, $body, $headers)) {
+        echo "<p class='sent-message'>Mensagem enviada com sucesso!</p>";
     } else {
-        echo "Error sending message!";
+        echo "<p class='error-message'>Erro ao enviar a mensagem. Por favor, tente novamente.</p>";
     }
 } else {
-    echo "Invalid request!";
+    echo "<p class='error-message'>Método de requisição inválido.</p>";
 }
 ?>
